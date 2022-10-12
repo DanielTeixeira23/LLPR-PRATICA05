216fb9484291a1ef2817ba51ad1b84c59ca821e5
DanielTeixeira23 <115299209+DanielTeixeira23@users.noreply.github.com>
2022-10-12 11:10:26 -0300

    Add files via upload

---
+++ Pratica 7.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese_Brazil");
+	
+	printf("Mensagem de Saudação \n\n");
+	
+	printf("# # # # # # # # # # # # # # # # # # # # \n");
+	printf("#                                     # \n");
+	printf("#                                     # \n");
+	printf("#         SEJA BEM-VINDO              # \n");
+	printf("#                                     # \n");
+	printf("#                                     # \n");         
+	printf("# # # # # # # # # # # # # # # # # # # # \n"); 
+	
+	printf("\n\n");
+	
+	return 0;
+}
